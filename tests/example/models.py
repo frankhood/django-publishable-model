2bcc7fcc19a960436750afbd060a22949899c43a
Antonio Mustich <a.mustich@frankhood.it>
2022-10-10 15:49:01 +0200

    Fix imports

--- tests/example/models.py
+++ tests/example/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-#TODO from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
 
 from publishable_model.models import PublishableModel
 
 
 class News(PublishableModel, models.Model):
-    title = models.CharField(_("Title"), max_length=64, )
-    content = models.TextField(_("Content"), blank=True, default="")
+    title = models.CharField(
+        "Title",
+        max_length=64,
+    )
+    content = models.TextField("Content", blank=True, default="")
 
     def __str__(self):
         return self.title
 
     class Meta(PublishableModel.Meta):
-        verbose_name = _('News')
-        verbose_name_plural = _('News')
+        """News Meta."""
+
+        verbose_name = "News"
+        verbose_name_plural = "News"
